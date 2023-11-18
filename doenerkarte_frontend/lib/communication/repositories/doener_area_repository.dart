d0d92214a28aef76f702391d8023c4f0e7b775a1
Daniel Lenhardt <ckwdani@gmail.com>
2023-11-18 18:29:14 +0100

    add search field

--- doenerkarte_frontend/lib/communication/repositories/doener_area_repository.dart
+++ doenerkarte_frontend/lib/communication/repositories/doener_area_repository.dart
@@ -12,10 +12,7 @@ class DoenerAreaRepository{
   /// gets list of all doeners in area
   Future<List<Doener>> getDoenerFromArea(Area area) async {
     var  response = await backendCommunicator.post('doener_in_bounding_box', body: area.toJson());
-    var list = response.map((e) {
-      return Doener.fromJson(e as Map<String, dynamic>);
-    });
-    list = response.map((e) => Doener.fromJson(e as Map<String, dynamic>)).toList();
-    return  list.toList() as List<Doener>;
+    var list = response.map((e) => Doener.fromJson(e as Map<String, dynamic>));
+    return  list.toList();
   }
 }
--- doenerkarte_frontend/lib/pages/mainMap/map_main.dart
+++ doenerkarte_frontend/lib/pages/mainMap/map_main.dart
@@ -1,8 +1,10 @@
+
 import 'package:doenerkarte/communication/entities/Doener.dart';
 import 'package:doenerkarte/communication/repositories/doener_area_repository.dart';
 import 'package:doenerkarte/core/core_widgets/main_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geocode/geocode.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -17,17 +19,16 @@ class MapMain extends StatefulWidget {
 class _MapMainState extends State<MapMain> {
 
   MapController mapController = MapController();
+  Future<List<Doener>>? doeners;
+  LatLngBounds? currentVisibleBounds;
+  TextEditingController searchController = TextEditingController();
+
   @override
   void initState() {
-    Future<List<Doener>> doeners = DoenerAreaRepository().getDoenerFromArea(Area(48.00, 48.1372,  11.00, 11.5755));
-    doeners.then((value) {
-      print(doeners);
-    });
 
     super.initState();
     mapController.mapEventStream.listen((event) {
-      var visibleBounds = event.camera.visibleBounds;
-      print("${visibleBounds.east}, ${visibleBounds.north}, ${visibleBounds.south}, ${visibleBounds.west}");
+      currentVisibleBounds = event.camera.visibleBounds;
     });
   }
 
@@ -63,6 +64,33 @@ class _MapMainState extends State<MapMain> {
               Container(
                   color: Colors.blue,
                   child: IconButton(onPressed: loadDoener, icon: Icon(Icons.no_food))),
+              FutureBuilder(
+                future: doeners,
+                builder: (context, snapshot) => buildForSnapshot(snapshot),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                child: TextField(
+                  controller: TextEditingController(),
+
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () => searchAdress(searchController.text),
+                        icon: Icon(Icons.search)),
+
+                    fillColor: Colors.white,
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelText: 'Search',
+                  ),),
+              )
             ],
           ),
       ),
@@ -70,10 +98,71 @@ class _MapMainState extends State<MapMain> {
   }
 
 
+  searchAdress(String stringAdress){
+    GeoCode geoCode = GeoCode();
+    geoCode.forwardGeocoding(address: stringAdress).then((value) {
+      if(value.latitude == null || value.longitude == null){
+        return;
+      }else{
+        mapController.move(LatLng(value.latitude!, value.longitude!), 15);
+        loadDoener();
+      }
+    });
+  }
+
+  buildForSnapshot(AsyncSnapshot<List<Doener>> snapshot) {
+    if(snapshot.connectionState == ConnectionState.waiting){
+      return CircularProgressIndicator();
+    }
+    if (snapshot.hasData) {
+      return MarkerLayer(
+          markers: snapshot.data!.map((e) => Marker(
+            point: LatLng( e.lat , e.lon ),
+            width: 60,
+            height: 60,
+            child: GestureDetector(
+              onTap: () =>  showBottomSheet(context, e),
+              child: Container(
+                child: Column(
+                  children: [
+                    Container(
+                      color: Colors.grey.withOpacity(0.3),
+                        child: Text("${(e.priceCents.toDouble() / 100).toStringAsFixed(2)}€", style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF900000)),)),
+                    Icon(Icons.location_on, size: 40,),
+                  ],
+                ),
+              ),
+            ),
+          )).toList()
+      );
+    } else if (snapshot.hasError) {
+      return Text("${snapshot.error}");
+    }
+    if(doeners == null){
+      return Container();
+    }
+    return const CircularProgressIndicator();
+  }
+
+
+
+  void showBottomSheet(BuildContext context, Doener doener){
+    showModalBottomSheet(
+      showDragHandle: true,
+        context: context, builder: (c1){
+      return Container(
+        child: ListTile(
+          title: Text(doener.name),
+          subtitle: Text("distance: 5000000km"),
+          trailing: Text("${(doener.priceCents.toDouble() / 100)}€"),
+        ),
+      );
+    });
+  }
+
   void loadDoener() {
-    Future<List<Doener>> doeners = DoenerAreaRepository().getDoenerFromArea(Area(48.00, 48.1372,  11.00, 11.5755));
-    doeners.then((value) {
-      print(doeners);
+    setState(() {
+      doeners = DoenerAreaRepository().getDoenerFromArea(Area(   currentVisibleBounds?.south??0, currentVisibleBounds?.north??0, currentVisibleBounds?.west??0, currentVisibleBounds?.east??0));
     });
   }
 }
